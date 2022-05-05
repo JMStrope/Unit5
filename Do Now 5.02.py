e2422951a2e034ea2eb2c23b63aba164ae963cab
Jack <Jmstrope24@students.bmchs.org>
2022-05-05 09:30:44 -0500

    ADDED

---
+++ Do Now 5.02.py
@@ -0,0 +1,57 @@
+'''
+############
+Do Now 5.02
+############
+
+1. In your Console
+------------------
+Type the following:
+
+my_dictionary = {
+'kittens': 'cute animals'
+}
+my_dictionary['kittens'] = 'p. cute'
+print(my_dictionary)
+
+In your Notebook
+----------------
+Respond to the following:
+
+Write down what the 2nd line does.
+The second line reassigns kittens in dictionary a new definition 
+
+
+2. In your Console
+------------------
+my_dictionary = {}
+my_dictionary['puppies'] = 'baby dogs'
+print(my_dictionary)
+
+Continue in your Notebook
+-------------------------
+Respond to the following prompt
+Write down what the 2nd line does.
+
+adds a new term and assigns a new definiton as well
+
+3. In your Console
+my_dictionary = {
+'kittens': 'cute animals',
+'puppies': 'baby dogs'
+}
+​
+my_dictionary.pop('kittens')
+print(my_dictionary)
+my_dictionary.pop('bunnies')
+my_dictionary.pop('bunnies', None)
+Continue In your Notebook
+Write down what the second line does.
+
+.pop removes the term and its defintion completely
+
+What is different between my_dictionary.pop('bunnies') and my_dictionary.pop('bunnies', None)?
+bunnies is the whole term and none is just the word bunnies
+'''
+
+
+
