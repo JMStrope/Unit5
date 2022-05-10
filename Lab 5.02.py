dbab69a7152f3a6aeae276991adc55cdadfd4f70
Jack <Jmstrope24@students.bmchs.org>
2022-05-10 09:21:46 -0500

    ADDED

--- Lab 5.02.py
+++ Lab 5.02.py
@@ -54,7 +54,11 @@ example_word_list = example_paragraph_lower_no_punctuation.split(" ")
  
  
  
-example_paragraph = "It was a beautiful day in New York City. Our hero Ariana Grande was on a walk from the Standard to Duane Reade. Ariana Grande was walking rather quickly because she had lived in New York for a few months. All of a sudden a slimy donut appeared out of nowhere. Ariana Grande decided to prance foolishly instead of dealing with the situation. Thrown off from Duane Reade Ariana Grandedecides to go to Times Square instead. What a beautiful day in New York."
+example_paragraph = '''It was a beautiful day in New York City. Our hero Ariana Grande was on a walk 
+from the Standard to Duane Reade. Ariana Grande was walking rather quickly because she had lived in 
+New York for a few months. All of a sudden a slimy donut appeared out of nowhere. Ariana Grande decided 
+to prance foolishly instead of dealing with the situation. Thrown off from Duane Reade Ariana Grande 
+decides to go to Times Square instead. What a beautiful day in New York.'''
 
 
 #remove all uppercase letters
@@ -62,10 +66,30 @@ example_paragraph_lower = example_paragraph.lower()
 
 #remove all periods
 example_paragraph_lower_no_punctuation = example_paragraph_lower.replace(".", "")
-
+example_paragraph_lower_no_punctuation = example_paragraph_lower_no_punctuation.replace("\n", " ")
 #convert paragraph into a list of individual strings
 example_word_list = example_paragraph_lower_no_punctuation.split(" ")
 
-print(example_paragraph)
 
-dictionary = {example_paragraph}
+
+dictionary = {}
+
+for word in example_word_list:
+    if word in dictionary:
+        dictionary[word] += 1
+    else:
+        dictionary[word] = 1
+
+
+
+
+while True:
+    print(dictionary)
+    user_choice = input("What would you like to know the frequency of? (enter 'q' to quit) > ")
+
+    if user_choice in dictionary:
+        print(f"{user_choice} appears {dictionary[user_choice]} times.")
+    elif user_choice == 'q':
+        break
+    else:
+        print("That does not appear at all.")
